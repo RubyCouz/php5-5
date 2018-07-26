38b9369a1cf3e7528e7b8a4f188910eccb936a93
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-26 18:25:16 +0200

    exo5

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 5</title>
+  </head>
+  <body>
+    <p>
+<?php
+//création du tableau month
+$country array (
+  02 => 'Aisne',
+  59 => 'Nord',
+  60 => 'Oise',
+  62 => 'Pas-de-Calais',
+  80 => 'Somme');
+?>
+    </p>
+  </body>
+</html>
